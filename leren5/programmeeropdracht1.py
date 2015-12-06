5155507d6c9b67ae1d951d8094a97125534be9f5
Yzonix <yzonix@gmail.com>
2015-12-06 11:20:03 +0100

    comments leren5 programmeer

--- leren5/programmeeropdracht1.py
+++ leren5/programmeeropdracht1.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+# Initialize with training data and the possible class values
+# EXAMPLE:
+# gnn = GausianNaiveBayes([[1 2 1] [5 2 0] [0 4 1]], [0, 1])
+# gnn.posteriors([1 2])
+# gnn.accuracy
 class GausianNaiveBayes():
     def __init__(self, training_data, class_values):
         self.training_data = training_data
@@ -11,6 +16,7 @@ class GausianNaiveBayes():
         new_training_data = np.delete(training_data, indexes, 1)
         return new_training_data
 
+    # Splits data in sublists by class
     def _splitdata(self):
         splitted_data_list = []
         for y in self.class_values:
@@ -18,55 +24,68 @@ class GausianNaiveBayes():
             splitted_data_list.append(split)
         return splitted_data_list
 
+    # Computes the mean and var columnwise of an vector
     def _mean_and_var(self, array):
         array = np.delete(array, -1, 1) # remove class column
         means = np.mean(array, axis=0)
         variances = np.var(array, axis=0)
         return means, variances
 
+    # Computes the prior by dividing the size of the class vector by the size of the vector
+    # of all classes
     def _prior(self, array):
         size_array, _ = array.shape
         prior = size_array / self.training_data_size
         return prior
 
+    # Computes the probability density function given sigma, mu and an test example value
     def pdf(self, mean, variance, test_value):
         exps = (( -(test_value - mean) ** 2) / (2 * variance **2))
         return ( exps / np.sqrt(2 * np.pi * variance ** 2) )
 
     def posteriors(self, test_data_row):
-        test_data_row = np.delete(test_data_row, -1) # remove class column
-        split_by_class = self._splitdata()
+        test_data_row = np.delete(test_data_row, -1) # remove class column (TODO: should be before this function)
+        split_by_class = self._splitdata() # Get a list of class vectors.
         pdf_values_all_classes = []
         prior_values_all_classes = []
 
+        # Loop over the class vectors separately
         for class_data in split_by_class:
-            class_means, class_variances = self._mean_and_var(class_data)
+            class_means, class_variances = self._mean_and_var(class_data) # Get the mean and variance of an class vector
 
-            variance_zero_list = []
-            for i, variance in enumerate(class_variances):
-                if variance == 0:
-                    variance_zero_list.append(i)
+            # Generates a list of columns where the variance is zero
+            # variance_zero_list = []
+            # for i, variance in enumerate(class_variances):
+            #    if variance == 0:
+            #        variance_zero_list.append(i)
 
             pdf_values_class = 1
+            # Loop over all features of a test example
             for mean, variance, test_value in zip(class_means, class_variances, test_data_row):
                 pdf_value = self.pdf(mean, variance, test_value)
-                #if np.isnan(pdf_value) or pdf_value == 0: # FUCK THIS SHIT
-                #    pdf_value = 1
                 pdf_values_class *= pdf_value
-            pdf_values_all_classes.append(pdf_values_class)
 
+            # Stores the pdf and prior values as an element in a list
+            pdf_values_all_classes.append(pdf_values_class)
             prior_values_all_classes.append(self._prior(class_data))
 
+        # Store the values above the dividing sign as element by class
         teller_values = []
         for pdf_value_class, prior_value in zip(pdf_values_all_classes, prior_values_all_classes):
             teller_values.append( pdf_value_class * prior_value )
 
-        posteriors = []
+        # Sum the evidence
         evidence = sum(teller_values)
+
+        # Store all posteriors by class an element in list
+        posteriors = []
         for value in teller_values:
             posteriors.append( value / evidence )
+
         return posteriors
 
+    # Computes the accuracy percentage by dividing the amount of correctly guessed classes by the total amount
+    # of classes in the test-set.
     def accuracy(self, test_data):
         counter = 0
         totalrows, _ = test_data.shape
@@ -78,6 +97,7 @@ class GausianNaiveBayes():
             print(test_data[i][-1], (max_class + 1))
         return (counter / totalrows) * 100
 
+# Reads a csv-file into a vector given a file name.
 def read_file(csvfilename):
     array = np.loadtxt(csvfilename, delimiter=';', skiprows=0, dtype=np.float64)
     return array
@@ -87,7 +107,7 @@ if __name__ == "__main__":
     test_array = read_file("digist123-2.csv")
 
     def _remove_zero_features(training_data, test_data):
-        indexes = np.where(~training_data.any(axis=0))[0]
+        #indexes = np.where(~training_data.any(axis=0))[0]
         new_training_data = np.delete(training_data, [0, 7, 8, 15, 16, 23, 24, 31, 32, 33, 39, 40, 47, 48, 56, 63], axis=1)
         new_test_data = np.delete(test_data, [0, 7, 8, 15, 16, 23, 24, 31, 32, 33, 39, 40, 47, 48, 56, 63], axis=1)
         return new_training_data, new_test_data
