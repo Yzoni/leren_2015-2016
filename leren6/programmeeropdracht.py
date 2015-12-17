e2f2f4b06d08153571beb142c5e8c38f6e7ab93c
Yzonix <yzonix@gmail.com>
2015-12-17 19:45:11 +0100

    final changes programmeeropdrach 6

--- leren6/programmeeropdracht.py
+++ leren6/programmeeropdracht.py
@@ -107,18 +107,9 @@ def read_file(csvfilename, xcolumns, ycolumn):
     y = array[:,ycolumn]
     return x, y
 
-def true_mean_dataset(xdata, ydata):
-    dicts = dict.fromkeys(list(set(ydata)), [])
-    for x, y in zip(xdata, ydata):
-        dicts[y].append(x)
-    means = {}
-    for d in dicts:
-        means[d] = np.mean(dicts[d], axis=0)
-    return means
-
 digits1_x, digits1_y = read_file("digist123-1.csv", list(range(0, 64)), 64)
-print("NOTE: if this error occurs: 'ValueError: need at least one array to concatenate', please just run the script"
-      "again untill it works.")
+print("NOTE: if this error occurs: 'ValueError: need at least one array to concatenate', please just run the script "
+      "again until it works.")
 
 # QUESTION 1: k-means clusterin implementation
 print("\nQUESTION 1")
@@ -129,7 +120,7 @@ pprint.pprint(kmeans.means())
 # QUESTION 2: k optimization
 print("\nQUESTION 2")
 print("NOTE: if this error occurs: 'ValueError: need at least one array to concatenate', please just run the script"
-      " again untill it works.")
+      " again until it works.")
 print("NOTE: close matplotwindow to continue...")
 kmeans_optimal = KMeans(digits1_x, 3)
 example_dict = kmeans_optimal.learn(15, plot_error=1)
@@ -168,10 +159,23 @@ for class_element in example_dict:
         list_rows_one_class.append(i[0])
     found_classes.append(list_rows_one_class)
 print("Found classes by original line number: " + str(found_classes))
-# Manually setting class to found clusters:
-found_class1 = [0, 1, 3, 4, 5, 6, 7, 8, 9, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 50, 52, 53, 55, 56, 57, 58, 59, 60, 61, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 237, 276, 277, 278, 289, 291]
-found_class2 = [2, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 101, 102, 103, 104, 105, 106, 108, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 154, 156, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 175, 177, 179, 181, 182, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196]
-found_class3 = [13, 49, 51, 54, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 107, 109, 129, 149, 150, 151, 152, 153, 155, 157, 174, 176, 178, 180, 183, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288, 290, 292, 293, 294, 295, 296, 297, 298]
+# Manually setting class to found clusters (This can't be dynamically done because a we are using dicts and
+# dictionaries are not sorted):
+found_class1 = [0, 1, 3, 4, 5, 6, 7, 8, 9, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
+                42, 43, 44, 45, 46, 47, 48, 50, 52, 53, 55, 56, 57, 58, 59, 60, 61, 75, 76, 77, 78, 79, 80, 81, 82, 83,
+                84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 237, 276, 277, 278, 289, 291]
+found_class2 = [2, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 101, 102, 103, 104, 105, 106, 108, 110, 111, 112,
+                113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 130, 131, 132, 133,
+                134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 154, 156, 158, 159, 160,
+                161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 175, 177, 179, 181, 182, 184, 185,
+                186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196]
+found_class3 = [13, 49, 51, 54, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 107, 109, 129, 149, 150, 151,
+                152, 153, 155, 157, 174, 176, 178, 180, 183, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207,
+                208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227,
+                228, 229, 230, 231, 232, 233, 234, 235, 236, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248,
+                249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268,
+                269, 270, 271, 272, 273, 274, 275, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288, 290, 292, 293,
+                294, 295, 296, 297, 298]
 count1 = 0
 
 for c in found_class1:
@@ -196,7 +200,6 @@ print("Accuracy of: " + str((count1 + count2 + count3) / len(digits1_y) * 100))
 # QUESTION 4:
 # find_anomalies returns the examples of which the distance to the mean is
 # greater than the threshold distance. These examples are the anomalies
-print("\nQuestion 4")
 anomalies = kmeans_optimal.find_anomalies(example_dict, 40)
 
 # QUESTION 5:
